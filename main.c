f22619d154e790bfdca1270941cb81bdc8dfca1b
Sai <saishravanm@gmail.com>
2024-10-08 13:43:43 -0500

    Completed Barebones UI

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ void draw_box(int width, int y, int x, const char* label){
     mvaddch(y + 1, x + width, '+');
 }
 
-bool isHover(Button button, int mouse_x, int mouse_y){
+bool isClick(Button button, int mouse_x, int mouse_y){
     if(mouse_x >= button.x && mouse_x <= button.x + button.width && mouse_y >= button.y && mouse_y <= button.y + button.height){
         return true;
     }
@@ -38,59 +38,68 @@ bool isHover(Button button, int mouse_x, int mouse_y){
 void draw_button(int y, int x, const char* label, bool is_selected){
     int width = 24; 
 
-    if(is_selected){
+    if(is_selected == true){
         attron(A_REVERSE);
     }
 
     draw_box(width,y, x, label);
 
-    if(is_selected){
+    if(!is_selected == false){
         attroff(A_REVERSE);
     }
 
 }
-int main(){
+int main() {
     Button buttons[NUM_OPTIONS] = {
-        {10,4,24,3},
-        {10,8,24,3},
-        {10,12,24,3},
-        {10,16,24,3}
+        {10, 4, 24, 3},
+        {10, 8, 24, 3},
+        {10, 12, 24, 3},
+        {10, 16, 24, 3}
     };
     MEVENT event;
     int highlight = 0;
-    int ch; 
+    int ch;
+
     initscr();
     noecho();
     cbreak();
-    keypad(stdscr,true);
+    keypad(stdscr, true);
     mousemask(ALL_MOUSE_EVENTS, NULL);
     curs_set(1);
-    
-    while(1){
-        clear(); 
-        for(int i = 0; i < NUM_OPTIONS; i++){
-            draw_button(buttons[i].y, buttons[i].x, menu_options[i], i==highlight);
-        }
 
-        refresh();
+    // Draw all buttons initially
+    for (int i = 0; i < NUM_OPTIONS; i++) {
+        draw_button(buttons[i].y, buttons[i].x, menu_options[i], i == highlight);
+    }
 
+    refresh();
+
+    // Main loop
+    while (1) {
         ch = getch();
-        switch(ch){
-        case KEY_MOUSE:
-            if(getmouse(&event) == OK){
-                if(event.bstate && BUTTON1_CLICKED){
-                    for(int i = 0; i < NUM_OPTIONS; i++){
-                        if(isHover(buttons[i],event.x, event.y)){
-                                mvprintw(22, 10, "Clicked on: %s", menu_options[i]);
+        switch (ch) {
+            case KEY_MOUSE:
+                if (getmouse(&event) == OK) {
+                    if (event.bstate & BUTTON1_CLICKED) {
+                        for (int i = 0; i < NUM_OPTIONS; i++) {
+                            if (isClick(buttons[i], event.x, event.y)) {
+                                // Unhighlight the previously highlighted button
+                                draw_button(buttons[highlight].y, buttons[highlight].x, menu_options[highlight], false);
+
+                                // Highlight the newly clicked button
                                 highlight = i;
-                                refresh();
-                                getch();
+                                draw_button(buttons[highlight].y, buttons[highlight].x, menu_options[highlight], true);
+                                refresh();  // Update the screen
+
                                 break;
                             }
                         }
                     }
                 }
+                break;
         }
     }
+
+    endwin();  // End ncurses mode properly
     return 0;
 }
